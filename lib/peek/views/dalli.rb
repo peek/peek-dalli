f8baa2fe23b7599dff70efc2715525ca5a9f36a9
dewski <me@garrettbjerkhoel.com>
2013-06-23 21:00:59 -0700

    Keep track of hits and misses

--- lib/peek/views/dalli.rb
+++ lib/peek/views/dalli.rb
@@ -7,6 +7,9 @@ module Peek
         @duration = Atomic.new(0)
         @calls = Atomic.new(0)
 
+        @hits = Atomic.new(0)
+        @misses = Atomic.new(0)
+
         setup_subscribers
       end
 
@@ -19,8 +22,18 @@ module Peek
         end
       end
 
+      def context
+        {
+          :hits => @hits.value,
+          :misses => @misses.value
+        }
+      end
+
       def results
-        { :duration => formatted_duration, :calls => @calls.value }
+        {
+          :duration => formatted_duration,
+          :calls => @calls.value
+        }
       end
 
       private
@@ -30,6 +43,16 @@ module Peek
         before_request do
           @duration.value = 0
           @calls.value = 0
+          @hits.value = 0
+          @misses.value = 0
+        end
+
+        subscribe('cache_hit.active_support') do
+          @hits.update { |value| value + 1 }
+        end
+
+        subscribe('cache_miss.active_support') do
+          @misses.update { |value| value + 1 }
         end
 
         subscribe(/cache_(.*).active_support/) do |name, start, finish, id, payload|
