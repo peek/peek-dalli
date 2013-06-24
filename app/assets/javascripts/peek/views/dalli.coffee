0e8f25ec40302c6b326b5a304eea22a67dee706a
dewski <me@garrettbjerkhoel.com>
2013-06-24 16:21:55 -0700

    Only subscribe to active_support

--- app/assets/javascripts/peek/views/dalli.coffee
+++ app/assets/javascripts/peek/views/dalli.coffee
@@ -1,6 +1,11 @@
-$(document).on 'peek:render', (event, request_id, data) ->
+$(document).on 'peek:render', (event, requestId, data) ->
+  title = []
+  title.push("Reads: #{data.context.dalli.reads}")
+  title.push("Misses: #{data.context.dalli.misses}")
+  title.push("Writes: #{data.context.dalli.writes}")
+
   $('#peek-dalli-tooltip')
-    .attr('title', "Hits: #{data.context.dalli.hits}<br />Misses: #{data.context.dalli.misses}")
+    .attr('title', title.join('<br>'))
     .tipsy
       html: true
       gravity: $.fn.tipsy.autoNS
--- lib/peek/views/dalli.rb
+++ lib/peek/views/dalli.rb
@@ -7,8 +7,9 @@ module Peek
         @duration = Atomic.new(0)
         @calls = Atomic.new(0)
 
-        @hits = Atomic.new(0)
+        @reads = Atomic.new(0)
         @misses = Atomic.new(0)
+        @writes = Atomic.new(0)
 
         setup_subscribers
       end
@@ -24,8 +25,9 @@ module Peek
 
       def context
         {
-          :hits => @hits.value,
-          :misses => @misses.value
+          :reads => @reads.value,
+          :misses => @misses.value,
+          :writes => @writes.value
         }
       end
 
@@ -43,16 +45,22 @@ module Peek
         before_request do
           @duration.value = 0
           @calls.value = 0
-          @hits.value = 0
+
+          @reads.value = 0
           @misses.value = 0
+          @writes.value = 0
         end
 
-        subscribe('cache_hit.active_support') do
-          @hits.update { |value| value + 1 }
+        subscribe('cache_read.active_support') do
+          @reads.update { |value| value + 1 }
         end
 
         subscribe('cache_miss.active_support') do
           @misses.update { |value| value + 1 }
+        end
+
+        subscribe('cache_write.active_support') do
+          @writes.update { |value| value + 1 }
         end
 
         subscribe(/cache_(.*).active_support/) do |name, start, finish, id, payload|
